0e119aa550660cb226529aea20137241dff6345f
caiosidesx <118694590+caiosidesx@users.noreply.github.com>
2026-05-06 18:48:42 -0300

    Próxima etapa: Dar funcionalidade ao botão do Whatsapp do formulário

--- scripts.js
+++ scripts.js
@@ -4,3 +4,10 @@ const navbar = document.querySelector(".navbar")
 menuBtn.addEventListener("click", () => {
     navbar.classList.toggle("active")
 })
+
+navbar.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+       navbar.classList.remove('active')
+    })
+})
+
